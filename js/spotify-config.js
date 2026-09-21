6a261e850b6eea97b499a704955141a4a8bef2f8
aldi-web969 <needbapak@gmail.com>
2026-09-22 02:23:43 +0700

    Update spotify-config.js

--- js/spotify-config.js
+++ js/spotify-config.js
@@ -1,1 +1,8 @@
-
+/* Spotify OAuth configuration.
+   Replace SPOTIFY_CLIENT_ID with the Client ID from Spotify Developer Dashboard.
+   The Redirect URI must exactly match your registered URI. */
+window.SPOTIFY_CONFIG = {
+  clientId: 'PASTE_YOUR_SPOTIFY_CLIENT_ID_HERE',
+  redirectUri: window.location.origin + window.location.pathname,
+  scopes: ['user-read-private', 'user-read-email']
+};
