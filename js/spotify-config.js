52677f74384ca93f647a6a42204eed3455b44422
aldi-web969 <needbapak@gmail.com>
2026-09-22 03:26:33 +0700

    Update spotify-config.js

--- js/spotify-config.js
+++ js/spotify-config.js
@@ -2,7 +2,11 @@
    Replace SPOTIFY_CLIENT_ID with the Client ID from Spotify Developer Dashboard.
    The Redirect URI must exactly match your registered URI. */
 window.SPOTIFY_CONFIG = {
-  clientId: 'a5dd486ff00a4694b004bb9b3d583591',
-  redirectUri: window.location.origin + window.location.pathname,
-  scopes: ['user-read-private', 'user-read-email']
+    clientId: "a5dd486ff00a4694b004bb9b3d583591",
+    redirectUri: "https://aldi-web969.github.io/XI-IPS-1/
+",
+    scopes: [
+        "user-read-private",
+        "user-read-email"
+    ]
 };
